a020d051e868fac147e1e641e9127fd5177984ff
hiichloee <reta1iate@hotmail.com>
2025-04-14 03:43:37 +1000

    Simulate test failure

--- src/test/java/sit707_week9/TaskInboxServiceTest.java
+++ src/test/java/sit707_week9/TaskInboxServiceTest.java
@@ -8,23 +8,26 @@ public class TaskInboxServiceTest {
 	
     private TaskInboxService service = new TaskInboxService();
 
+    // Existing ID
     @Test
-    public void testGetTasksForExistingStudent() {
+    public void testGetTasksForExistingStudent() {  
         System.out.println("+++ Get tasks for existing student ID +++");
         List<Task> tasks = service.getTasksForStudent("222113273");
-        assertEquals(2, tasks.size());
+        assertEquals(999, tasks.size());
         System.out.println("--- Task 1: " + tasks.get(0).getTitle());
         System.out.println("--- Task 2: " + tasks.get(1).getTitle() + "\n");
     }
 
+    // Non-existing ID
     @Test
-    public void testGetTasksForNonExistingStudent() {
+    public void testGetTasksForNonExistingStudent() { 
         System.out.println("+++ Get tasks for non-existing student ID +++");
         List<Task> tasks = service.getTasksForStudent("000000000");
         assertTrue(tasks.isEmpty());
         System.out.println("--- No tasks found as expected ---\n");
     }
 
+    // Empty String
     @Test
     public void testGetTasksWithEmptyStudentId() {
         System.out.println("+++ Get tasks with empty student ID +++");
@@ -33,6 +36,7 @@ public class TaskInboxServiceTest {
         System.out.println("--- Returned empty list ---\n");
     }
 
+    // null value
     @Test
     public void testGetTasksWithNullStudentId() {
         System.out.println("+++ Get tasks with null student ID +++");
@@ -41,13 +45,15 @@ public class TaskInboxServiceTest {
         System.out.println("--- Returned empty list ---\n");
     }
 
+    // Data Consistency
     @Test
     public void testTaskDetailsAreConsistent() {
         System.out.println("+++ Check task count vs details consistency +++");
         List<Task> tasks = service.getTasksForStudent("222113273");
         assertEquals(tasks.size(), tasks.stream().map(Task::getTitle).count());
     }
-
+    
+    // Batch Query Performance
     @Test
     public void testMultipleDummyStudentIds() {
         System.out.println("+++ Loop through 50 dummy IDs +++");
